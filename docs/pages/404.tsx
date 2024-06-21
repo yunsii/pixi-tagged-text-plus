64211561213b92eddf72f5db4ee0bd768e5a1fcc
Yuns <yuns.xie@qq.com>
2024-06-21 14:32:21 +0800

    Initial commit

---
+++ docs/pages/404.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function Component404() {
+  const navigate = useNavigate()
+
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page not found</p>
+      <button
+        onClick={() =>
+          navigate('/', {
+            replace: true,
+          })}
+      >
+        Home
+      </button>
+    </div>
+  )
+}
+
+export default Component404
---
+++ vite.lib.config.ts
@@ -0,0 +1,35 @@
+import path from 'node:path'
+
+import { mergeConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+import type { UserConfig } from 'vite'
+
+import baseConfig from './vite.base.config'
+
+// https://vitejs.dev/config/
+export default mergeConfig(baseConfig, {
+  plugins: [dts({
+    exclude: '**/demos',
+  })],
+  build: {
+    minify: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // inspired from: https://github.com/vitejs/vite/discussions/1736#discussioncomment-2621441
+      // preserveModulesRoot: https://rollupjs.org/guide/en/#outputpreservemodulesroot
+      output: {
+        dir: 'dist',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
+        entryFileNames: '[name].js',
+      },
+      // ref: https://github.com/vitejs/vite/issues/4454#issuecomment-1407461535
+      external: (source, _, isResolved) => !(isResolved || /(^[./])|(^@\/)/.test(source)),
+    },
+    target: 'esnext',
+  },
+} as UserConfig)
